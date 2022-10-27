361043f170ce744f5b933072e7e432055f3bf2b5
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-10-27 17:28:17 +0600

    nav added

---
+++ src/layout/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Pages/shared/Header/Nav';
+
+const Main = () => {
+    return (
+        <div className='px-20 py-4'>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Main;
